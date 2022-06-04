ddd02c6ffceba0207855740414405b22411a8a63
Banda | Developer, Designer <56312496+mmurima-kai@users.noreply.github.com>
2022-06-04 05:35:09 +0300

    Task

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,7 +15,7 @@ int main(void)
     } else if (n == 0) {
         printf("%d is zero",n);
     }
-    else {printf("%d is negative",n)};
+    else {printf("%d is negative",n);}
     
 	return (0);
 }
